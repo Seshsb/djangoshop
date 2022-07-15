8bf1fab6a33a7e538ad82ee2cd4cc872a813c4c0
Seshsb <ruslanlee00@gmail.com>
2022-07-16 01:04:37 +0500

    first_store

--- eshop/settings.py
+++ eshop/settings.py
@@ -125,7 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
